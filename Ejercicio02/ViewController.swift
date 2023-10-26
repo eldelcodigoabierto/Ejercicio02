d5b9c93be7d2b9ddf7fb577ca86e9d9d69495ebc
alexroig <alex.roigronda@cieep.com>
2023-10-26 09:15:45 +0200

    Initial Commit

---
+++ Ejercicio02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ejercicio02
+//
+//  Created by Alex Roig  on 26/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
